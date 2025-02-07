0cdcf96b615e51ce102edf597a14552f8cc7a0bc
BhaveshPandya2009 <bhaveshpandya2009@gmail.com>
2025-02-07 09:23:57 +0000

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -16,7 +16,7 @@ function Dashboard() {
           <iframe
             title="Grafana"
             /*src="https://snapshots.raintank.io/dashboard/snapshot/SqiZMlt7aBFzIOM2maGXmXoJKiQ63pNw?orgId=0&refresh=5s&theme=light"*/
-            src="https://snapshots.raintank.io/dashboard/snapshot/jniwJm8PQ7qt0GYUj53LdIBjRBh1pnYz?orgId=0&refresh=5s&theme=light"
+            src="https://snapshots.raintank.io/dashboard/snapshot/JDnUC1v7WlBIV9V9pWVGnRCEqXhyEoJs?orgId=0&refresh=5s&theme=light"
             width="100%"
             height="1000"
             frameBorder="0"
